948eb06372865ec8e0d1c9d6768dbdea08b8634e
pacio017 <pacio017@umn.edu>
2023-04-21 22:11:31 -0500

    Conducted thorough unit tests: DataCollection CSV

--- libs/transit/src/DataCollectionSingleton.cc
+++ libs/transit/src/DataCollectionSingleton.cc
@@ -9,7 +9,7 @@ DataCollectionSingleton* DataCollectionSingleton::getInstance(){
 }
 
 void DataCollectionSingleton::writeTimeInfo(IEntity* entity, float time){
-    std::string type = (entity->GetDetails())["type"];
+    std::string type = (entity->GetDetails())["type"].ToString();
     if (type.compare("drone") == 0) {
         droneTimeInfo[(Drone*)entity] = time;
         return;
@@ -18,7 +18,7 @@ void DataCollectionSingleton::writeTimeInfo(IEntity* entity, float time){
 }
 
 void DataCollectionSingleton::writeDistanceInfo(IEntity* entity, float distance){
-    std::string type = (entity->GetDetails())["type"];
+    std::string type = (entity->GetDetails())["type"].ToString();
     if (type.compare("drone") == 0) {
         droneDistanceInfo[(Drone*)entity] = distance;
         return;
@@ -27,7 +27,7 @@ void DataCollectionSingleton::writeDistanceInfo(IEntity* entity, float distance)
 }
 
 void DataCollectionSingleton::writeAccountInfo(IEntity* entity, double accountBalance){
-    std::string type = (entity->GetDetails())["type"];
+    std::string type = (entity->GetDetails())["type"].ToString();
     if (type.compare("drone") == 0) {
         droneAccountInfo[(Drone*)entity] = accountBalance;
         return;
@@ -47,28 +47,27 @@ void DataCollectionSingleton::writeToCSV(){
     
     // *** Open csv file ***
     std::ofstream data;
-    
-    data.open ("CSV/simData.csv");
+    data.open ("simData101.csv", std::ios::trunc);
     std::string row = ""; // Acc for writing a row to .csv
 
     // *** Top Axis ***
-    data << "Name, Time, Distance, Account($), Battery Usage, Number of Trips\n";
+    data << "ID, Time, Distance, Account($), Battery Usage, Number of Trips\n";
 
     // *** Gets data for all Drones and enters it into a .csv file ***
     int count = 1;
     for (std::map<Drone*, double>::iterator entry = droneAccountInfo.begin(); entry != droneAccountInfo.end(); ++entry){
         Drone* drone = entry->first;
-        row.append("Drone #" + std::to_string(count));
+        row.append("Drone #" + std::to_string(drone->GetId()));
         row.append(",");
-        // row.append(std::to_string(droneTimeInfo[drone]));
-        // row.append(",");
-        // row.append(std::to_string(droneDistanceInfo[drone]));
-        // row.append(",");
+        row.append(std::to_string(droneTimeInfo[drone]));
+        row.append(",");
+        row.append(std::to_string(droneDistanceInfo[drone]));
+        row.append(",");
         row.append(std::to_string(droneAccountInfo[drone]));
-        // row.append(",");
-        // row.append(std::to_string(batteryUsage[drone]));
-        // row.append(",");
-        // row.append(std::to_string(numberOfTrips[drone]));
+        row.append(",");
+        row.append(std::to_string(batteryUsage[drone]));
+        row.append(",");
+        row.append(std::to_string(numberOfTrips[drone]));
         row.append("\n");
         data << row;
         count++;
@@ -79,12 +78,12 @@ void DataCollectionSingleton::writeToCSV(){
     count = 1;
     for (std::map<Robot*, double>::iterator entry = robotAccountInfo.begin(); entry != robotAccountInfo.end(); ++entry){
         Robot* robot = entry->first;
-        row.append("Robot #" + std::to_string(count));
+        row.append("Robot #" + std::to_string(robot->GetId()));
         row.append(",");
-        // row.append(std::to_string(robotTimeInfo[robot]));
-        // row.append(",");
-        // row.append(std::to_string(robotDistanceInfo[robot]));
-        // row.append(",");
+        row.append(std::to_string(robotTimeInfo[robot]));
+        row.append(",");
+        row.append(std::to_string(robotDistanceInfo[robot]));
+        row.append(",");
         row.append(std::to_string(robotAccountInfo[robot]));
         row.append(",");
         row.append("N/A,N/A");
